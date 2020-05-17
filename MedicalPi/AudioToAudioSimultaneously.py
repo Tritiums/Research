1045ffd675b8554eff156ddb8817fa049ef971aa
Chuan Yang <tritiums@outlook.com>
2020-05-17 22:26:50 +0800

    Update

--- MedicalPi/AudioToAudioSimultaneously.py
+++ MedicalPi/AudioToAudioSimultaneously.py
@@ -7,24 +7,53 @@
 import pyaudio
 import numpy as np
 
-#The following code comes from markjay4k as referenced below
 
-chunk=4096
-RATE=44100
+# In[ ]:
 
-p=pyaudio.PyAudio()
 
-#input stream setup
-stream=p.open(format = pyaudio.paInt16,rate=RATE,channels=1, input_device_index = 2, input=True, frames_per_buffer=chunk)
+# The following code comes from markjay4k as referenced below
+chunk = 512
+RATE = 44100
 
-#the code below is from the pyAudio library documentation referenced below
+
+# In[ ]:
+
+
+# Define the object
+p = pyaudio.PyAudio()
+
+
+# In[ ]:
+
+
+# input stream setup
+stream = p.open(format = pyaudio.paInt16,
+                rate=RATE,
+                channels=1, 
+                input_device_index = 2, 
+                input=True, 
+                frames_per_buffer=chunk)
+
+# the code below is from the pyAudio library documentation referenced below
 #output stream setup
-player=p.open(format = pyaudio.paInt16,rate=RATE,channels=1, output=True, frames_per_buffer=chunk)
+player = p.open(format = pyaudio.paInt16,
+                rate=RATE,
+                channels=1, 
+                output=True, 
+                frames_per_buffer=chunk)
+
+
+# In[ ]:
+
 
 while True:            #Used to continuously stream audio
-     data=np.fromstring(stream.read(chunk,exception_on_overflow = False),dtype=np.int16)
-     player.write(data,chunk)
-    
+    data = np.fromstring(stream.read(chunk,exception_on_overflow = False),dtype=np.int16)
+    player.write(data,chunk)
+
+
+# In[ ]:
+
+
 #closes streams
 stream.stop_stream()
 stream.close()
